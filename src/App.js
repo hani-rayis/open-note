4dfdab742d88b148892c4c6882fc8e004a5f519b
Hani Rayis <hani.arayis@gmail.com>
2021-03-25 23:01:33 +0800

    added redirect to router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import SignIn from "./auth/Signin";
 import SignUp from "./auth/Signup";
 import Dashboard from "./dashboard/Dashboard";
@@ -11,6 +11,9 @@ class App extends Component {
       <BrowserRouter>
         <div className="app-container">
           <Switch>
+            <Route exact path="/">
+              <Redirect to="/signin" />
+            </Route>
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
